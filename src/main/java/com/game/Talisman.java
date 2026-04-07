a7c8d82ec68e7a7a0da2aa00b2100fa842c5b9b5
dorukozkann <dorukzkn@gmail.com>
2026-04-07 17:01:09 +0300

    updated talisman, equip and unequip metod

--- src/main/java/com/game/Talisman.java
+++ src/main/java/com/game/Talisman.java
@@ -3,32 +3,70 @@ package com.game;
 public class Talisman extends Item {
     
     public enum TalismanType {
-        TALISMAN_5("/sprites/items/talisman-5.png", "", "", 1, 0.05),
-        TALISMAN_4("/sprites/items/talisman-4.png", "", "", 1, 0.10),
-        TALISMAN_3("/sprites/items/talisman-3.png", "", "", 1, 0.20),
-        TALISMAN_2("/sprites/items/talisman-2.png", "", "", 1, 0.30),
-        TALISMAN_1("/sprites/items/talisman-1.png", "", "", 1, -1);
+        TALISMAN_5("/sprites/items/talisman-5.png", "Purple Gem", "Increases walk speed", 1, 0.10, 0, 0, 0, 0, 0.8),
+        TALISMAN_4("/sprites/items/talisman-4.png", "Shadow Ring", "Boosts everything", 1, 0.02, 40, 10, 10, 0.4, 1),
+        TALISMAN_3("/sprites/items/talisman-3.png", "Armor Ring", "Increases Armor", 1, 0.20, 0, 5, 0, 0, 0),
+        TALISMAN_2("/sprites/items/talisman-2.png", "Blue Charm", "Increases damage and speed", 1, 0.30, 0, 0, 10, 0.2, 0),
+        TALISMAN_1("/sprites/items/talisman-1.png", "Gold Ring", "Increases max health.", 1, 0.10, 25, 0, 0, 0, 0);
 
         public final String imagePath;
         public final String name;
         public final String description;
         public final double cooldownDuration;
         public final double baseChance;
+        public final double healthBonus;
+        public final double armorBonus;
+        public final double damageBonus;
+        public final double attackSpeedBonus;
+        public final double walkSpeedBonus;
 
-        TalismanType(String imagePath, String name, String description, double cooldownDuration, double baseChance) {
+        TalismanType(String imagePath, String name, String description, double cooldownDuration, double baseChance, double healthBonus, double armorBonus, double damageBonus,
+                     double attackSpeedBonus, double walkSpeedBonus) {
             this.imagePath = imagePath;
             this.name = name;
             this.description = description;
             this.cooldownDuration = cooldownDuration;
             this.baseChance = baseChance;
+            this.healthBonus      = healthBonus;
+            this.armorBonus       = armorBonus;
+            this.damageBonus      = damageBonus;
+            this.attackSpeedBonus = attackSpeedBonus;
+            this.walkSpeedBonus   = walkSpeedBonus;
         }
     }
 
     private TalismanType talismanType;
+    public boolean isEquipped = false;
 
     public Talisman(TalismanType type) {
         super(type.imagePath, type.name, type.description, type.cooldownDuration);
         this.talismanType = type;
+    }
+
+    public void equip(LivingEntity target){
+        if(isEquipped) {
+            return;
+        }
+        isEquipped = true;
+        target.maxHealth   += talismanType.healthBonus;
+        target.health      += talismanType.healthBonus;
+        target.armor       += talismanType.armorBonus;
+        target.damage      += talismanType.damageBonus;
+        target.attackSpeed += talismanType.attackSpeedBonus;
+        target.walkSpeed   += talismanType.walkSpeedBonus;
+    }
+
+    public void unequip(LivingEntity target){
+        if(!isEquipped) {
+            return;
+        }
+        isEquipped = false;
+        target.maxHealth   -= talismanType.healthBonus;
+        target.health       = Math.min(target.health, target.maxHealth); // if max health is lower than health
+        target.armor       -= talismanType.armorBonus;
+        target.damage      -= talismanType.damageBonus;
+        target.attackSpeed -= talismanType.attackSpeedBonus;
+        target.walkSpeed   -= talismanType.walkSpeedBonus;
     }
 
     public static Talisman randomTalisman(double luckFactor) {
